8be8a8f15c07b9c253664a648ed5386b230342a7
udit_tiwari <udit.tiwari@quantive.com>
2023-08-28 20:26:37 +0530

    completed the first react app

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,11 +1,15 @@
 import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+import Backdrop from './components/Backdrop';
 
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text='Passing first prop' />
+      <Todo text='Passing second prop' />
+      <Todo text='Different props for different Todo' />
     </div>
   );
 }
